9a03ffbb4e04345479f9abfa1c8023c390c93306
那須元忠 <nasumototada@nasumototadanoMacBook-Pro.local>
2020-07-23 21:28:35 +0900

    lan

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function() {
+Route::get('index', 'ContactFormController@index')->name('contact.index');
+Route::get('create', 'ContactFormController@create')->name('contact.create');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
